902bbdf29c2ee7173075f0d4a6f7a2d26024b364
Rhyse Simpson <skittles9832@gmail.com>
2021-01-27 22:43:12 +1100

    Clean up code a bit and run through all current players on the leaderboard

--- leaderboards.py
+++ leaderboards.py
@@ -1,4 +1,4 @@
-import urllib.request, json 
+import urllib.request, json
 
 region = "AP"
 URL = f"https://dgxfkpkb4zk5c.cloudfront.net/leaderboards/affinity/{region}/queue/competitive/act/97b6e739-44cc-ffa7-49ad-398ba502ceb0?startIndex=0&size=0"
@@ -9,12 +9,27 @@ with urllib.request.urlopen(f"{URL}") as url:
 newSize = data['totalPlayers']
 newIndex = 0
 rank = 1
-while newIndex < 500:
-    newURL = f"https://dgxfkpkb4zk5c.cloudfront.net/leaderboards/affinity/{region}/queue/competitive/act/97b6e739-44cc-ffa7-49ad-398ba502ceb0?startIndex={newIndex}&size={newSize}"
-    with urllib.request.urlopen(f"{newURL}") as url2:
-        data = json.loads(url2.read().decode())
-        for s in range(len(data['players'])):
-            print(f"{rank}" + ". " + data['players'][s]['gameName'] + "#" + str(data['players'][s]['tagLine']) + " | RR: " + str(data['players'][s]['rankedRating']) +
-                 " | Wins: " + str(data['players'][s]['numberOfWins']))
-            rank += 1
-    newIndex += 10
+search = False
+broken = False
+
+def getLeaderboard(newIndex, newSize, rank, search, broken):
+    while newIndex < newSize:
+        newURL = f"https://dgxfkpkb4zk5c.cloudfront.net/leaderboards/affinity/{region}/queue/competitive/act/97b6e739-44cc-ffa7-49ad-398ba502ceb0?startIndex={newIndex}&size={newSize}"
+        with urllib.request.urlopen(f"{newURL}") as url2:
+            data = json.loads(url2.read().decode())
+            for s in range(len(data['players'])):
+                if search:
+                    if data['players'][s]['gameName'] == f"{search}":
+                        print(f"{rank}" + ". " + data['players'][s]['gameName'] + "#" + str(data['players'][s]['tagLine']) + " | RR: " + str(data['players'][s]['rankedRating']) +
+                            " | Wins: " + str(data['players'][s]['numberOfWins']))
+                        broken = True
+                        break
+                else:
+                    print(f"{rank}" + ". " + data['players'][s]['gameName'] + "#" + str(data['players'][s]['tagLine']) + " | RR: " + str(data['players'][s]['rankedRating']) +
+                        " | Wins: " + str(data['players'][s]['numberOfWins']))
+                rank += 1
+        newIndex += 10
+        if broken == True:
+            break
+
+getLeaderboard(newIndex, newSize, rank, search, broken)
